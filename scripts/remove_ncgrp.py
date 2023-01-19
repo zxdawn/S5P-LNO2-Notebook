96c01e2f45db0c1b3d423d8d9c5c7257d5e2d6c5
zxdawn <xinzhang1215@gmail.com>
2023-01-19 15:37:35 +0800

    upload script of removing nc group

---
+++ scripts/remove_ncgrp.py
@@ -0,0 +1,22 @@
+'''
+Remove the "Lightning" group of NetCDF file
+'''
+
+import os
+from netCDF4 import Dataset
+
+filename = '../data/lno2/S5P_LNO2_lifetime.nc'
+rootGrp = Dataset(filename)
+
+# get the group names of Cases
+cases = list(sorted(rootGrp.groups.keys()))
+
+grps = []
+for case in cases:
+    swaths = list(rootGrp[case].groups.keys())
+    grp = [case+'/'+swath+'/Lightning' for swath in swaths]
+    grps.extend(grp)
+
+savename = filename.replace('.nc', '_subset.nc')
+print(f'Deleted the "Lightning" subgroups and saved to {savename}')
+os.system(f"ncks -O -x -C -g {','.join(grps)} {filename} {savename}")
